d243ce43ccacf3c273027c035ce3698f37d83bfc
zfkun <zfkun@msn.com>
2013-10-15 23:33:27 +0800

    update
    update

--- src/button.js
+++ src/button.js
@@ -9,11 +9,34 @@ define( function ( require ) {
 
     var Control = require( 'saber-control' );
 
-    var Button = function( options ) {};
-
-    Button.prototype = {
-        constructor: Button
+    /**
+     * 按钮控件
+     * 
+     * @extends module:Control
+     * @requires Control
+     * @exports Button
+     */
+    var Button = function() {
+        Button.superClass.constructor.apply( this, arguments );
     };
 
+    Button.prototype = {
+
+        /**
+         * 控件类型标识
+         * 
+         * @type {string}
+         * @private
+         */
+        type: 'Button',
+
+        initialize: function() {
+            console.info( 'Button inited', this.superClass );
+        }
+
+    };
+
+    Control.inherits( Button );
+
     return Button;
-} );
+});
--- test/button.test.js
+++ test/button.test.js
@@ -1,9 +1,37 @@
 define(function() {
 
-    var Cookie = require( 'saber-button' );
+	Control = require( 'saber-control' );
+    Button = require( 'saber-button' );
 
     describe( 'Button', function() {
         // TODO
+        
+        describe( 'should create instance of Button' , function() {
+
+        	b1 = new Button({
+        		onBeforeinit: function() {
+        			console.info( 'beforeinit', arguments );
+        		},
+        		onAfterinit: function() {
+        			console.info( 'afterinit', arguments );
+        		},
+        	});
+        	b1.on( 'enable', function() {
+        		console.info( 'enable', arguments );
+        	}).on( 'disable', function() {
+        		console.info( 'disable', arguments );
+        	});
+
+        	b1.enable();
+        	b1.disable();
+        	b1.enable();
+
+
+        	console.info( b1 );
+
+        	// it().toEqual(  );
+
+        });
     });
 
 });
