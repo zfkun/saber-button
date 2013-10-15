d8eabe27efa44b39e1152becfe41dfe6a8b4db08
zfkun <zfkun@msn.com>
2013-10-15 15:50:27 +0800

    up
    up

---
+++ src/button.js
@@ -0,0 +1,19 @@
+/**
+ * Saber
+ * 
+ * @file 按钮控件
+ * @author zfkun(zfkun@msn.com)
+ */
+
+define( function ( require ) {
+
+    var Control = require( 'saber-control' );
+
+    var Button = function( options ) {};
+
+    Button.prototype = {
+        constructor: Button
+    };
+
+    return Button;
+} );
